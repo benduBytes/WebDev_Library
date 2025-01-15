21c9418e7fb343ce158b7f362c75da935bf37ff9
benedict <benedict1426paul@gmail.com>
2025-01-15 22:50:52 +0530

    SECTION-17 : The Dicee Game

---
+++ SECTION-17/Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img1").setAttribute("src", "./images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src", "./images/dice"+randomNumber2+".png");
+
+function winner(randomNumber1, randomNumber2){
+    if(randomNumber1 > randomNumber2){
+        return "🚩 Player 1 Wins!";
+    }else if(randomNumber1 < randomNumber2){
+        return "Player 2 Wins! 🚩";
+    }else{
+        return "Draw!";
+    }
+}
+
+document.querySelector("h1").innerHTML = winner(randomNumber1, randomNumber2);
